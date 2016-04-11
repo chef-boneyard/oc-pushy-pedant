46488e6479d9a7bae4d8088fb0ae3f1252d67460
Scott Christopherson <scott@scott-christopherson.com>
2016-04-11 18:45:45 -0400

    WIP: trying to get all tests to pass

--- spec/pushy/support/end_to_end_util.rb
+++ spec/pushy/support/end_to_end_util.rb
@@ -23,11 +23,11 @@ shared_context "end_to_end_util" do
 
   # A variety of timeouts are used to ensure that jobs have started,
   # nodes are available, etc.  These are set very conservatively.
-  let (:client_start_timeout) { 5 }
+  let (:client_start_timeout) { 5 * 10 }
   let (:job_start_timeout) { 30 }
-  let (:job_status_timeout_default) { 30 }
-  let (:node_availability_timeout) { 10 }
-  let (:node_status_timeout) { 10 }
+  let (:job_status_timeout_default) { 30 * 10 }
+  let (:node_availability_timeout) { 10 * 10 }
+  let (:node_status_timeout) { 10 * 10 }
   let (:server_restart_timeout) { 45 } # increasing this makes failing tests take longer, but salvages some slow runs
 
   let (:client_creation_retries) { 5 }  # how many times to retry a client creation
